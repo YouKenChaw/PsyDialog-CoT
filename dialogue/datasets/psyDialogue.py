a78bc09ebd9f89f7cad02107537ea8bf1ff5f719
YouKen <YougenZhou@outlook.com>
2023-09-14 10:52:41 +0800

    update

--- dialogue/datasets/psyDialogue.py
+++ dialogue/datasets/psyDialogue.py
@@ -2,8 +2,8 @@ from torch.utils.data import Dataset
 
 
 class PsyDialogueCotDataset(Dataset):
-    def __int__(self, data_dir, tokenizer):
-        super().__int__()
+    def __init__(self, data_dir, tokenizer):
+        super().__init__()
 
     def __getitem__(self, index):
         pass
