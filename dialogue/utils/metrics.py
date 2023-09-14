9b29fd82062ec6f6d881f4e0987f9a6de47ad9a7
YouKen <YougenZhou@outlook.com>
2023-09-14 16:47:51 +0800

    finish the training code

---
+++ dialogue/utils/metrics.py
@@ -0,0 +1,36 @@
+import torch
+import torch.distributed as dist
+
+
+class Metric:
+    def __init__(self, device):
+        self.n_step = 0
+        self.right = torch.Tensor([0]).to(device=device)
+        self.total = torch.Tensor([0]).to(device=device)
+        self.total_loss = torch.Tensor([0]).to(device=device)
+        self.world_size = dist.get_world_size()
+
+    def __call__(self, logits, labels, loss):
+        return self.update(logits, labels, loss)
+
+    def update(self, logits, labels, loss):
+        self.n_step += 1
+        with torch.no_grad():
+            shift_preds = logits[..., :-1, :].argmax(dim=-1)
+            shift_labels = labels[..., 1:]
+            self.right += (shift_preds == shift_labels).masked_fill(shift_labels.eq(-100), 0).sum().item()
+            self.total += (shift_labels != -100).sum().item()
+            self.total_loss += loss.item()
+
+    def get_metric(self, reset=True):
+        dist.all_reduce(self.right, op=dist.ReduceOp.SUM)
+        dist.all_reduce(self.total, op=dist.ReduceOp.SUM)
+        dist.all_reduce(self.total_loss, op=dist.ReduceOp.SUM)
+        acc = (self.right / self.total).item()
+        loss = self.total_loss.item() / (self.world_size * self.n_step)
+        if reset:
+            self.n_step = 0
+            self.right.fill_(0)
+            self.total.fill_(0)
+            self.total_loss.fill_(0)
+        return acc, loss
